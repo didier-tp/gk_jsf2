da40958a516cec52eaff5a7a9914bffee9ec5b3b
didier <didier@d-defrance.fr>
2017-11-10 14:20:12 +0100

    avec primefaces

---
+++ src/main/java/tp/data/Adresse.java
@@ -0,0 +1,30 @@
+package tp.data;
+
+public class Adresse {
+	private String numeroEtRue;
+	private String codePostal;
+	private String ville;
+	
+
+	//+get/set
+	public String getNumeroEtRue() {
+		return numeroEtRue;
+	}
+	public void setNumeroEtRue(String numeroEtRue) {
+		this.numeroEtRue = numeroEtRue;
+	}
+	public String getCodePostal() {
+		return codePostal;
+	}
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+	public String getVille() {
+		return ville;
+	}
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	
+	
+}
---
+++ src/main/java/tp/web/jsf/mbean/InscriptionMBean.java
@@ -0,0 +1,60 @@
+package tp.web.jsf.mbean;
+
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import tp.data.Adresse;
+
+@ManagedBean //JSF sans spring
+@RequestScoped // ou bien @SessionScoped
+public class InscriptionMBean {
+
+	private Adresse adresse; //à saisir
+	private String nom;
+	private Date date;
+	
+	private String message; //à afficher
+	
+	public String inscrire(){
+		message = "inscription prise en compte pour nom="
+				+nom + "date=" + date + "adresse="+ adresse;
+		return null; //pour affichage message sur meme page
+	}
+
+	public Adresse getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(Adresse adresse) {
+		this.adresse = adresse;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+	
+}
